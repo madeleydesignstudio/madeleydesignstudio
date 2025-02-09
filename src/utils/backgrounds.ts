80bf01873bc651c0a51795d4eb8404ec9a381ee1
mxdeley <dev@madeleydesignstudio.org>
2025-02-09 01:01:54 +0000

    added animation

--- src/utils/backgrounds.ts
+++ src/utils/backgrounds.ts
@@ -4,6 +4,10 @@ const backgrounds = [
   `${IMAGEKIT_BASE}/background-1.png?updatedAt=1739058183985`,
   `${IMAGEKIT_BASE}/background-2.png?updatedAt=1739058203974`,
   `${IMAGEKIT_BASE}/background-3.png?updatedAt=1739058220606`,
+  `${IMAGEKIT_BASE}/background-4.png?updatedAt=1739062779911`,
+  `${IMAGEKIT_BASE}/background-5.png?updatedAt=1739062804098`,
+  `${IMAGEKIT_BASE}/background-6.png?updatedAt=1739062828837`,
+  `${IMAGEKIT_BASE}/background-7.png?updatedAt=1739062852970`,
 ] as const;
 
 export function getRandomBackground(): string {
