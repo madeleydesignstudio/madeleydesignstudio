7c1d115a85c611365716e3adab018228840dd8e4
danielcmadeley <daniel@madeleydesignstudio.org>
2025-02-16 22:04:18 +0000

    added payload blogs

--- src/app/hooks/useWordAnimations.ts
+++ src/app/hooks/useWordAnimations.ts
@@ -4,7 +4,7 @@ export const useWordAnimations = (words: string[], startScroll: number) => {
   const { scrollY } = useScroll();
 
   // Pre-create all transform values before mapping
-  return words.map((_, i) => {
+  return words.map((word, i) => {
     const start = startScroll + i * 20;
     const end = startScroll + 100 + i * 20;
     return useTransform(scrollY, [start, end], [0, 1]);
