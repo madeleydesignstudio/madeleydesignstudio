d25134702ebdb732459cb71e29bf3cf545ac0c34
danielcmadeley <daniel@madeleydesignstudio.org>
2025-02-16 18:48:36 +0000

    added favicon

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,6 +1,5 @@
 // storage-adapter-import-placeholder
 import { postgresAdapter } from "@payloadcms/db-postgres";
-import { payloadCloudPlugin } from "@payloadcms/payload-cloud";
 import { lexicalEditor } from "@payloadcms/richtext-lexical";
 import path from "path";
 import { buildConfig } from "payload";
@@ -32,8 +31,5 @@ export default buildConfig({
     },
   }),
   sharp,
-  plugins: [
-    payloadCloudPlugin(),
-    // storage-adapter-placeholder
-  ],
+  plugins: [],
 });
