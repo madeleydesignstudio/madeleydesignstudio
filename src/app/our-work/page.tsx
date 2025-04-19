69f02fbb0be5cd7e0ec3526616039679c6e93b90
danielcmadeley <daniel@madeleydesignstudio.org>
2025-04-19 21:11:46 +0200

    added projects

---
+++ src/app/our-work/page.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import Image from "next/image";
+
+const projects = [
+  { id: 1, name: "Project 1", image: "/01.png", category: "web" },
+  { id: 2, name: "Project 2", image: "/02.png", category: "brand" },
+  { id: 3, name: "Project 3", image: "/03.png", category: "web" },
+  { id: 4, name: "Project 4", image: "/04.png", category: "design" },
+  { id: 5, name: "Project 5", image: "/05.png", category: "web" },
+  { id: 6, name: "Project 6", image: "/06.png", category: "brand" },
+  { id: 8, name: "Project 8", image: "/08.png", category: "design" },
+  { id: 9, name: "Project 9", image: "/09.png", category: "web" },
+  { id: 10, name: "Project 10", image: "/10.png", category: "brand" },
+];
+
+const OurWork = () => {
+  return (
+    <section className="h-full p-2">
+      <div className="overflow-x-auto h-full overflow-y-hidden">
+        <div>our work</div>
+        <ul className="flex items-end h-full">
+          {projects.map((project) => (
+            <li
+              key={project.id}
+              className="flex-shrink-0 transition-transform hover:scale-105"
+            >
+              <div className="relative">
+                <Image
+                  src={project.image}
+                  alt={project.name}
+                  width={300}
+                  height={200}
+                />
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
+  );
+};
+export default OurWork;
--- src/components/providers/web-border.tsx
+++ src/components/providers/web-border.tsx
@@ -15,26 +15,26 @@ const WebBorder = ({ children }: { children: React.ReactNode }) => {
         {/* Corner crosses */}
         {/* Top-left */}
         <div className="absolute top-[29px] left-[19px] w-[3px] h-[3px] flex items-center justify-center z-30">
-          <div className="absolute w-[2px] h-[10px] rotate-45 bg-zinc-400" />
-          <div className="absolute w-[2px] h-[10px] -rotate-45 bg-zinc-400" />
+          <div className="absolute w-[1.5px] h-[12px] rotate-45 bg-zinc-400" />
+          <div className="absolute w-[1.5px] h-[12px] -rotate-45 bg-zinc-400" />
         </div>
 
         {/* Top-right */}
         <div className="absolute top-[29px] right-[19px] w-[3px] h-[3px] flex items-center justify-center z-30">
-          <div className="absolute w-[2px] h-[10px] rotate-45 bg-zinc-400" />
-          <div className="absolute w-[2px] h-[10px] -rotate-45 bg-zinc-400" />
+          <div className="absolute w-[1.5px] h-[12px] rotate-45 bg-zinc-400" />
+          <div className="absolute w-[1.5px] h-[12px] -rotate-45 bg-zinc-400" />
         </div>
 
         {/* Bottom-left */}
         <div className="absolute bottom-[29px] left-[19px] w-[3px] h-[3px] flex items-center justify-center z-30">
-          <div className="absolute w-[2px] h-[10px] rotate-45 bg-zinc-400" />
-          <div className="absolute w-[2px] h-[10px] -rotate-45 bg-zinc-400" />
+          <div className="absolute w-[1.5px] h-[12px] rotate-45 bg-zinc-400" />
+          <div className="absolute w-[1.5px] h-[12px] -rotate-45 bg-zinc-400" />
         </div>
 
         {/* Bottom-right */}
         <div className="absolute bottom-[29px] right-[19px] w-[3px] h-[3px] flex items-center justify-center z-30">
-          <div className="absolute w-[2px] h-[10px] rotate-45 bg-zinc-400" />
-          <div className="absolute w-[2px] h-[10px] -rotate-45 bg-zinc-400" />
+          <div className="absolute w-[1.5px] h-[12px] rotate-45 bg-zinc-400" />
+          <div className="absolute w-[1.5px] h-[12px] -rotate-45 bg-zinc-400" />
         </div>
 
         <div className="absolute top-[31px] left-[21px] right-[21px] bottom-[31px] overflow-auto h-[calc(100%-62px)] w-[calc(100%-42px)]">
