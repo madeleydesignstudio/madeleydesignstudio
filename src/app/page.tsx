e38eb38c9c9f5c129db287f844f2f998a2e16b08
danielcmadeley <daniel@madeleydesignstudio.org>
2025-04-21 11:09:45 +0200

    added homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Link from "next/link";
-
+import Image from "next/image";
+import { Separator } from "@/components/ui/separator";
 export default function Home() {
   return (
-    <div className="w-full h-screen ">
+    <div className="w-full h-full">
       <div className="p-2 h-full overflow-auto">
-        <div className=" w-full">
+        <div className=" w-full h-full flex flex-col gap-8">
           <h1 className="font-boska text-9xl text-zinc-50">
             madeleydesignstudio
           </h1>
@@ -26,6 +27,32 @@ export default function Home() {
             >
               Get In Touch
             </Link>
+          </div>
+          <div className=" flex justify-center items-center">
+            <Image
+              src="/01.png"
+              alt="Madeley Design Studio"
+              width={960}
+              height={540}
+            />
+          </div>
+          <div className="w-full flex justify-left items-center">
+            <p className="font-bricolage text-xl max-w-2xl text-zinc-50 font-extralight">
+              A Design & Development studio that is small by design, but
+              monolific by nature. We work with brands and companies to craft a
+              digital presence that captivates and resonates with its users.
+            </p>
+          </div>
+          <div className="w-[75%] mx-auto">
+            <Separator className="h-[0.5px] bg-zinc-50" />
+          </div>
+
+          <div className="w-full flex justify-left items-center">
+            <p className="font-bricolage text-xl max-w-2xl text-zinc-50 font-extralight">
+              A Design & Development studio that is small by design, but
+              monolific by nature. We work with brands and companies to craft a
+              digital presence that captivates and resonates with its users.
+            </p>
           </div>
         </div>
       </div>
