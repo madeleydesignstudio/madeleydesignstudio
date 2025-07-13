11117edb25b23bd3b2e4c836cbe98535b7f8b37c
danielcmadeley <daniel@madeleydesignstudio.org>
2025-07-13 18:56:20 +0100

    added ai image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="relative w-full h-full">
+    <div className="relative w-full h-full overflow-hidden">
       {/* Image in bottom left */}
       <div className="absolute bottom-0 left-0">
         <Image src="/ai-one.png" alt="AI One" width={650} height={850} />
@@ -16,7 +16,7 @@ export default function Home() {
           left: "33%",
           borderLeft: "600px solid transparent",
           borderRight: "600px solid transparent",
-          borderBottom: "1470px solid rgba(244, 244, 245, 0.1)",
+          borderBottom: "1470px solid rgba(244, 244, 245, 0.05)",
         }}
       />
     </div>
